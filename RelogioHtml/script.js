9cafd2f0d7c8b5de10163dd0aa767d48d07cc619
Francisco Wladison <francisco.wladison62@aluno.ifce.edu.br>
2023-09-02 20:42:05 -0300

    att

---
+++ RelogioHtml/script.js
@@ -0,0 +1,13 @@
+function updateClock() {
+    const now = new Date();
+    const hour = String(now.getHours()).padStart(2, '0');
+    const minute = String(now.getMinutes()).padStart(2, '0');
+    const second = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById('hour').textContent = hour;
+    document.getElementById('minute').textContent = minute;
+    document.getElementById('second').textContent = second;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
